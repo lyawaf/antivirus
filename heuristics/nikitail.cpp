a5271b64b9301abf3fadc5a58926b09bdd1ea672
d86leader <d86leader@gmail.com>
2017-02-24 17:38:57 +0700

    unfinished changes

--- heuristics/nikitail.cpp
+++ heuristics/nikitail.cpp
@@ -11,38 +11,57 @@ using std::set;
 
 const bool DEBUG = true;
 
+
+//sets of instructions that should be examined
+//with both arguments intresting
+set<string> good_doubly_operations {
+	 "add", "sub", "dec", "inc", "xor"
+};
+//with righthand argumant intresting
+set<string> good_rightly_operations {
+	"mov", "movb"
+};
+
+//a function that determines whether set contains element
+template <typename T, typename P>
+inline bool contains(set<T> s, P elem)
+{
+	return s.find(elem) != s.end();
+}
+
 //a function that given a disassembler will determine the registers used until the ret command or end of disassembly
 //first set is registers read, second is registers written
-pair< set<x86_reg>, set<x86_reg> > registersUsed(OneStepDisasm d)
+pair< set<x86_reg>, set<x86_reg> > registers_used(OneStepDisasm d)
 {
 	set<x86_reg> read {};
 	set<x86_reg> written {};
-	//if it's a jump or call command..
 	while (true)
 	{
 		auto instr = d.next();
-
 		if (instr.empty)
 			break;
-
 
 		if (DEBUG)
 			cout << "0x" <<hex << instr.address <<"\t\t" <<instr.mnemonic <<"\t" <<instr.operands <<endl;
 
-		if (instr.groups.find(CS_GRP_JUMP) != instr.groups.end())
+		if (contains(instr.groups, CS_GRP_JUMP))
 		{
 			cout << "found a jump command\n";
 		}
-		if (instr.groups.find(CS_GRP_CALL) != instr.groups.end())
+		else if (contains(instr.groups, CS_GRP_CALL))
 		{
 			cout << "found a call command\n";
 		}
-		if (instr.groups.find(CS_GRP_RET) != instr.groups.end())
+		else if (contains(instr.groups, CS_GRP_RET))
 		{
 			cout << "found a return command\n";
 
 			if (!DEBUG)
 				return make_pair(read, written);
+		}
+		else
+		{
+			if contains(good_doubly_operations, instr.)
 		}
 	}
 	return make_pair(read, written);
@@ -50,8 +69,7 @@ pair< set<x86_reg>, set<x86_reg> > registersUsed(OneStepDisasm d)
 
 CCTypes nikitailDeterminer(OneStepDisasm d)
 {
-	//do-while doesn't work for some reason, using this with break
-	auto t = registersUsed(d);
+	auto t = registers_used(d);
 	auto read = t.first;
 	auto written = t.second;
 
