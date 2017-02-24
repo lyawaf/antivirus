c928180e0cb31006b6caa48a11c755447205c938
d86leader <d86leader@gmail.com>
2017-02-24 21:43:39 +0700

    added maksimilian.h

---
+++ heuristics/maksimilian.h
@@ -0,0 +1,7 @@
+#include "wrapper.h"
+#include "cctypes.h"
+
+//Here is function that trying to determine CC, logic based on used_registers && ret argument
+
+//@@FUNCTION
+CCTypes maksimilian(const OneStepDisasm &);
