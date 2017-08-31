e002441ef2fb11ee5d93f34500459248a4d82043
d86leader <d86leader@github.com>
2017-08-31 15:24:58 +0700

    updated interface to the program

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <cstring>
 #include <stdexcept>
+#include <vector>
 
 #include <capstone/capstone.h>
 
@@ -13,28 +15,46 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	if (argc < 2)
+	if (argc < 3)
 	{
-		cerr <<"usage: " <<argv[0] <<" filename" <<endl;
+		cerr <<"Usage: " <<argv[0] <<" FILENAME ADDRESS [ADDRESS ...]" <<endl;
 		return 0;
 	}
-	string name {argv[1]};
+	string name (argv[1]);
+	vector<string> addresses (argv + 2, argv + argc);
 
-	try
+	for (auto i : addresses)
 	{
-		uint64_t t = 0x255d;
-		OneStepDisasm d {name, 64, t, 0x40255d};
-
-		auto type = determine(d);
-
-		switch (type)
+		try
 		{
-		default:
-			cout <<"whatever, idc\n";
+			auto addr   = stoll(i, nullptr, 16);
+			auto v_addr = addr + 0x400000; //a wild guess
+			OneStepDisasm d {name, 64, addr, v_addr};
+
+			auto type = determine(d);
+
+			switch (type)
+			{
+				case cdecl:
+					cout << "Function at " << i << ": cdecl" << endl;
+					break;
+				case stdcall:
+					cout << "Function at " << i << ": stdcall" << endl;
+					break;
+				case msfastcall:
+					cout << "Function at " << i << ": msfastcall" << endl;
+					break;
+				default:
+					cout <<"whatever, idc\n";
+			}
 		}
-	}
-	catch (runtime_error e)
-	{
-		cerr <<"exception caught: " <<e.what() <<endl;
+		catch (const invalid_argument& e)
+		{
+			cerr << "Could not convert \"" << i << "\"to address: " << e.what() << endl;
+		}
+		catch (const runtime_error& e)
+		{
+			cerr << "Exception caught when parsing argument \"" << i "\": " << e.what() <<endl;
+		}
 	}
 }
