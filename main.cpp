dc52ab4a70fd2d78d742d30ef14e14cd9e18b8d1
d86leader <d86leader@mail.com>
2017-09-02 10:08:42 +0700

    added functionality to accept parameters

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <stdexcept>
 #include <vector>
+#include <map>
 
 #include <capstone/capstone.h>
 
@@ -13,36 +14,84 @@
 
 using namespace std;
 
-int main(int argc, char** argv)
+typedef map<string, string> param_map;
+
+//parse argv's arguments of type --name=val to a nice map
+pair< param_map, vector<string> > get_parameters(const vector<string>& argv, param_map pars = param_map())
+{
+	vector<string> others;
+
+	for (auto param : argv)
+	{
+		if (param.size() > 2 && param[0] == '-' && param[1] == '-')
+		{
+			auto eq_index = param.find_first_of('=');
+			auto name_len = eq_index - 2;
+			auto name     = param.substr(2, name_len);
+			auto value    = param.substr(eq_index + 1);
+
+			pars[name] = value;
+		}
+		else
+		{
+			others.push_back(param);
+		}
+	}
+
+	return make_pair(pars, others);
+}
+
+void print_usage(const char* prgname)
+{
+	cerr <<"Usage: " <<prgname <<" FILENAME ADDRESS [ADDRESS ...]" <<endl;
+	return;
+}
+
+
+int main(int argc, char** __argv)
 {
 	if (argc < 3)
 	{
-		cerr <<"Usage: " <<argv[0] <<" FILENAME ADDRESS [ADDRESS ...]" <<endl;
+		print_usage(__argv[0]);
 		return 0;
 	}
-	string name (argv[1]);
-	vector<string> addresses (argv + 2, argv + argc);
+	vector<string> argv (__argv + 1, __argv + argc);
+	param_map default_params = {
+		{"arch", "32"}
+	};
+	auto t = get_parameters(argv, default_params);
 
-	for (auto i : addresses)
+	auto parameters = t.first;
+	auto args       = t.second;
+
+	//parse parameters to useful values
+	auto ARCH = stoi(parameters["arch"]);
+
+	//the syntax is that filename comes first
+	auto file_name  = args.front();
+
+	//set iteration at second string
+	auto iter = args.begin(); ++iter;
+	for (; iter != args.end(); ++iter)
 	{
 		try
 		{
-			auto addr   = stoull(i, nullptr, 16);
+			auto addr   = stoull(*iter, nullptr, 16);
 			auto v_addr = addr + 0x400000; //a wild guess
-			OneStepDisasm d {name, 32, addr, v_addr};
+			OneStepDisasm d (file_name, ARCH, addr, v_addr);
 
 			auto type = determine(d);
 
 			switch (type)
 			{
 				case cdecl:
-					cout << "Function at " << i << ": cdecl" << endl;
+					cout << "Function at " << *iter << ": cdecl" << endl;
 					break;
 				case stdcall:
-					cout << "Function at " << i << ": stdcall" << endl;
+					cout << "Function at " << *iter << ": stdcall" << endl;
 					break;
 				case msfastcall:
-					cout << "Function at " << i << ": msfastcall" << endl;
+					cout << "Function at " << *iter << ": msfastcall" << endl;
 					break;
 				default:
 					cout <<"whatever, idc\n";
@@ -50,15 +99,15 @@ int main(int argc, char** argv)
 		}
 		catch (const invalid_argument& e)
 		{
-			cerr << "Could not convert \"" << i << "\"to address: " << e.what() << endl;
+			cerr << "Could not convert \"" << *iter << "\"to address: " << e.what() << endl;
 		}
 		catch (const bad_alloc& e)
 		{
-			cerr << "Could not allocate space for file " << name << endl;
+			cerr << "Could not allocate space for file " << file_name << endl;
 		}
 		catch (const runtime_error& e)
 		{
-			cerr << "Exception caught when parsing argument \"" << i << "\": " << e.what() <<endl;
+			cerr << "Exception caught when parsing argument \"" << *iter << "\": " << e.what() <<endl;
 		}
 	}
 }
