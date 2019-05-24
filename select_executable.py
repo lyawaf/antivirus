e75d97698251f614c4eaea6632f4b282e4c43f18
Katerina <katrinelipatova@gmail.com>
2019-05-24 18:58:47 +0700

    correct re

--- select_executable.py
+++ select_executable.py
@@ -8,7 +8,7 @@ def select_executable(filepath, dirpath):
                                extract=True,
                                matryoshka=True,
                                directory=dirpath,
-                               dd='\b{0,1}(elf)|(mach-o)|(pe)\b')
+                               dd='[(,\b{0,1}](elf)|(mach-o)|(pe)[\b,)]')
 
     except binwalk.ModuleException as e:
         print("Critical failure:", e)
