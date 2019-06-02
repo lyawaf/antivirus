846cb7ae34be491d452f344b87084a91baa030d3
Katerina <katrinelipatova@gmail.com>
2019-06-02 16:57:38 +0700

    [-] exceptions catch

--- extract.py
+++ extract.py
@@ -2,16 +2,12 @@ import binwalk
 # dig for elf, mach-o, pe extracted files and if exists save it to /dirpath/_filename.extracted/
 
 def extract(filepath, dirpath):
-    try:
-        binwalk.scan('-D', 'elf::',
-                     '-D', 'microsoft executable:exe:',
-                     '-D', 'mach-o:o:',
-                     filepath, quiet=True,
-                               signature=True,
-                               extract=True,
-                               matryoshka=True,
-                               directory=dirpath)
-
-    except binwalk.ModuleException as e:
-        print("Critical failure:", e)
+    binwalk.scan('-D', 'elf::',
+                 '-D', 'microsoft executable:exe:',
+                 '-D', 'mach-o:o:',
+                 filepath, quiet=True,
+                           signature=True,
+                           extract=True,
+                           matryoshka=True,
+                           directory=dirpath)
 
