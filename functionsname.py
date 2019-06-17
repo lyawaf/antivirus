53cd67ce1ca75670548ff9d297823909eb07938e
lizatrif <lizatrif@gmail.com>
2019-06-17 22:51:25 +0700

    Update functionsname.py
    [~] beauty fix

--- functionsname.py
+++ functionsname.py
@@ -9,42 +9,39 @@ def start(filename):
     output, err = p.communicate()
     
     lines = output.decode('utf8').split('\n')
-    
-    n = len(lines)
 
-    sect=lines.index("Sections:")
-    symb=lines.index("SYMBOL TABLE:")
+    section_index = lines.index("Sections:")
+    symbols_index = lines.index("SYMBOL TABLE:")
 
-    name = ".text"
+    text_section_literal = ".text"
 
-    for i in range(sect + 2, symb, 2):
+    for i in range(section_index + 2, symbols_index, 2):
         words = lines[i].split(' ')
-        j=0
+        j = 0
         index = []
         for word in words:
             if word.strip():
                 index.append(j)
-            j = j+1
-        if words[index[1]]==name:
-            address=words[index[3]]
+            j = j + 1
+        if words[index[1]] == text_section_literal:
+            text_section_address = words[index[3]]
 
-    funcnames = []
-    funcaddress = []
+    functions_names = []
+    functions_address = []
 
-    for i in range(symb+1, n-3):
-        if lines[i].find(name) != -1:
+    for i in range(symbols_index + 1, len(lines) - 3):
+        if lines[i].find(text_section_literal) != -1:
             words = lines[i].split(' ')
             for word in words:
-                lastworld = word
-            funcnames.append(lastworld)
-            funcaddress.append(int(words[0],16)-int(address,16))
+                last_world = word
+            functions_names.append(last_world)
+            functions_address.append(int(words[0], 16) - int(text_section_address, 16))
 
-    print (funcaddress, funcnames)
+    print (functions_address, functions_names)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: {} file".format(sys.argv[0]))
         sys.exit(1)
     start(sys.argv[1])
-   
-
+    
