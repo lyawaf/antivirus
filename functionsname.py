a4fc3aa7070e183d8ef54af86bc89fec35e971ab
lizatrif <lizatrif@gmail.com>
2019-06-17 13:52:33 +0700

    Update functionsname.py
    [~] analysis for all section SYMBOLS TABLE

--- functionsname.py
+++ functionsname.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import re
+import sys
 from subprocess import Popen, PIPE
 
 def start(filename):
@@ -25,30 +26,27 @@ def start(filename):
                 index.append(j)
             j = j+1
         if words[index[1]]==name:
-            adress=words[index[3]]
-
-    i=0
-    for line in lines:
-        if "*ABS*" in line:
-            abs = i # abs - номер секции *ABS* в SYMBOL TABLE, отсюда начинаем разбирать функции:
-        i = i + 1
+            address=words[index[3]]
 
     funcnames = []
-    funcadress = []
+    funcaddress = []
 
-    for i in range(abs, n-3):
+    for i in range(symb+1, n-3):
         if lines[i].find(name) != -1:
             words = lines[i].split(' ')
             for word in words:
                 lastworld = word
             funcnames.append(lastworld)
-            funcadress.append(int(words[0],16)-int(adress,16))
+            funcaddress.append(int(words[0],16)-int(address,16))
 
-    print (funcadress, funcnames)
+    print (funcaddress, funcnames)
 
 if __name__ == "__main__":
+    '''
     if len(sys.argv) < 2:
         print("Usage: {} file".format(sys.argv[0]))
         sys.exit(1)
     start(sys.argv[1])
+    '''
+    start("nlbig")
 
