a35043d62fc22fd2e62004140a74b2b44c29cf65
lizatrif <lizatrif@gmail.com>
2019-06-17 23:38:25 +0700

    Update functionsname.py
    [*] modify list for functions and beauty fix

--- functionsname.py
+++ functionsname.py
@@ -13,35 +13,28 @@ def start(filename):
     section_index = lines.index("Sections:")
     symbols_index = lines.index("SYMBOL TABLE:")
 
-    text_section_literal = ".text"
+    text_literal = ".text"
 
     for i in range(section_index + 2, symbols_index, 2):
-        words = lines[i].split(' ')
-        j = 0
-        index = []
-        for word in words:
-            if word.strip():
-                index.append(j)
-            j = j + 1
-        if words[index[1]] == text_section_literal:
-            text_section_address = words[index[3]]
+        words = lines[i].split()
+        if words[1] == text_literal:
+            text_section_address = words[3]
 
-    functions_names = []
-    functions_address = []
+    functions = []
 
     for i in range(symbols_index + 1, len(lines) - 3):
-        if lines[i].find(text_section_literal) != -1:
+        if lines[i].find(text_literal) != -1:
             words = lines[i].split(' ')
-            for word in words:
-                last_world = word
-            functions_names.append(last_world)
-            functions_address.append(int(words[0], 16) - int(text_section_address, 16))
+            function_address = int(words[0], 16) - int(text_section_address, 16)
+            functions.append((words[-1], function_address))
 
-    print (functions_address, functions_names)
+    return (functions)
 
 if __name__ == "__main__":
+   
     if len(sys.argv) < 2:
         print("Usage: {} file".format(sys.argv[0]))
         sys.exit(1)
-    start(sys.argv[1])
-    
+    functions = start(sys.argv[1])
+    print (functions)
+
