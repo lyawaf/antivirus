d62ade00d9f500e63b577712b5012599cba301f1
d86leader <d86leader@mail.com>
2019-06-17 22:35:19 +0700

    [~] use native qt style

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from PyQt5.QtCore import QUrl
-from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtWidgets import QApplication
 from PyQt5.QtQml import QQmlApplicationEngine, qmlRegisterType
 import sys
 
@@ -9,7 +9,7 @@ from threatmodel import ThreatModel
 from scanworker import ScanWorker
 
 if __name__ == "__main__":
-    app = QGuiApplication(sys.argv)
+    app = QApplication(sys.argv)
 
     # register python classes in qml
     qmlRegisterType(ThreatModel, 'PyModels', 1, 0, 'ThreatModel')
