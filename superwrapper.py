944e308cdc9ffa60b6f63c00e8b659e49cc8df26
Yulia <juliajarskilli@gmail.com>
2019-06-20 10:46:03 +0700

    [+] logging

--- superwrapper.py
+++ superwrapper.py
@@ -6,20 +6,23 @@ from invoke_layer import invoke
 from os import listdir
 from os.path import isfile, join
 
-def scan_file(filename):
+def scan_file(filename, log):
+    log('Scanning {}...'.format(filename))
     try:
-        functions_addresses = get_functions_addresses(filename)
-        return (filename, invoke(filename, [addrs for (name, addrs) in functions_addresses]))
+        functions_addresses = [addrs for (name, addrs) in get_functions_addresses(filename)]
+        is_dangerous = invoke(filename, functions_addresses)
+        return (filename, is_dangerous)
     except Exception as exc:
         return (filename, True)
 
 
-def scan(filepath, dirpath):
+def scan(filepath, dirpath, log):
     extract(filepath, dirpath)
     extracted_dirpath = dirpath + '/_' + filepath + '.extracted'
-    files = [join(extracted_dirpath, f) for f in listdir(extracted_dirpath) if isfile(join(extracted_dirpath, f))]
+    full_path = join(extracted_dirpath, f)  
+    files = [full_path for f in listdir(extracted_dirpath) if isfile(full_path)]
     return list(map(scan_file, files))
 
 
 if __name__ == '__main__':
-    print(scan('elf_32_simple.bin', './CCPTool/tests'))
+    print(scan('elf_32_simple.bin', './CCPTool/tests', print))
